c0a5290be25188c8bcc3318780e4dc80e83bb52d
Natalia Arancibia Benavides <natalia.arancibia.be@gmail.com>
2017-12-14 10:33:57 -0300

    fotos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,7 @@
 $().ready(function() {
- 	   $("#Titulo").fadeIn(100).fadeOut(5000);
-      
- 
+ 	   $("#Titulo").fadeIn(5000).fadeOut(5000);
 });
 
- $(document).ready(function(){
-   setInterval(function(){
-   	$(".sel").show(500);
-   });
-   $('.FotosPlatos').hide();
-   $('.FotosPlatos:firts').show();
-   $('#Vegetariana').click(opcion);
-   $('#Vegana').click(opcion2);
-   $('#Exotica').click(opcion3);
-   $('#Pasteleria').click(opcion4);
 
-   function opcion(){
-   	$('#Vegetariana').show();
-   	$('#Vegana').hide();
-   	$('#Exotica').hide();
-   	$('#Pasteleria').hide();
-   }
-   function opcion1{
-   	$('#Vegana').show();
-   	$('#Exotica').hide();
-   	$('#Pasteleria').hide();
-   	$('Vegetariana').hide();
-
-   }
-   function opcion2();{
-   $('#Exotica').show();
-   $('#Pasteleria').hide();
-   $('#Vegetariana').hide();
-   $('#Vegana').hide();
- }
- function opcion3(){
-  $('#Pasteleria').show();
-  $('#Vegetariana').hide();
-  $('#Vegana').hide();
-  $('#Exotica').hide();
-}
-
-
-
- }
+ 
+  
